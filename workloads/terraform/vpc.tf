c9341f96995cc7680d26e8b7a638b3a622e85b58
mga <mga@example.com>
2022-04-22 20:24:12 +0200

    vpc.tf add provider and VPC for eu-west-1

--- workloads/terraform/vpc.tf
+++ workloads/terraform/vpc.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  alias  = "eu_west_1"
+  region = "eu-west-1"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -16,3 +21,25 @@ module "vpc" {
     Environment = "non-prod"
   }
 }
+
+module "vpc_eu_west_1" {
+  source = "terraform-aws-modules/vpc/aws"
+  providers = {
+    aws = aws.eu_west_1
+  }
+  name = "vpc-ff-workload1-1"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "non-prod"
+  }
+}
+
